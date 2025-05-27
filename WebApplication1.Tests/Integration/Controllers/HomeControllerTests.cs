0390d3c90c3541bcb00d318871dc91f9a989f9a8
recca0120 <recca0120@gmail.com>
2025-05-27 16:22:22 +0800

    refactor

--- WebApplication1.Tests/Integration/Controllers/HomeControllerTests.cs
+++ WebApplication1.Tests/Integration/Controllers/HomeControllerTests.cs
@@ -1,29 +1,17 @@
-// 移動自 ../../Integration/Controllers/HomeControllerIntegrationTests.cs
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Xunit;
 
-public class HomeControllerTests : IClassFixture<WebApplicationFactory<Program>>
+public class HomeControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly WebApplicationFactory<Program> _factory;
-
-    public HomeControllerTests(WebApplicationFactory<Program> factory)
-    {
-        _factory = factory;
-    }
+    private readonly WebApplicationFactory<Program> _factory = factory;
 
     [Theory]
     [InlineData("/")]
     [InlineData("/Home/Privacy")]
     public async Task Get_Endpoints_ReturnSuccessAndView(string url)
     {
-        // Arrange
         var client = _factory.CreateClient();
-
-        // Act
         var response = await client.GetAsync(url);
-
-        // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         var content = await response.Content.ReadAsStringAsync();
         Assert.False(string.IsNullOrWhiteSpace(content));
